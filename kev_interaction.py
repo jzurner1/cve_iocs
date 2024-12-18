a5b301af2f878f4b33e3db2475cdbcbb8d3b91c4
Joe Zurner <70181039+jzurner1@users.noreply.github.com>
2024-12-18 11:53:39 -0500

    Added docstring to get_kev_json()

--- kev_interaction.py
+++ kev_interaction.py
@@ -1,7 +1,11 @@
 import requests
 
 
-def get_kev_json():  # done
+def get_kev_json():
+    """ get raw KEV from CISA
+
+    :return: raw json
+    """
     kev_raw = requests.get("https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json")
     try:
         return kev_raw.json()

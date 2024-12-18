ab2be955993db7c5f8ce4b2b57a5728af734ba71
Joe Zurner <70181039+jzurner1@users.noreply.github.com>
2024-12-18 15:38:14 -0800

    ConnectionError fallback

--- kev_interaction.py
+++ kev_interaction.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def get_kev_json():
@@ -6,10 +7,18 @@ def get_kev_json():
 
     :return: raw json
     """
-    kev_raw = requests.get("https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json")
+    try:
+        kev_raw = requests.get("https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json")
+    except requests.exceptions.ConnectTimeout:
+        print('[-] connection failure in get_kev_json()\n')
+        return 1
+    except requests.exceptions.ConnectionError:
+        print('[-] connection failure in get_kev_json()\n')
+        return 1
+
     try:
         return kev_raw.json()
     except json.decoder.JSONDecodeError:
         print("[-] failed to decode KEV JSON data")
-        return 0
+        return 1
 
